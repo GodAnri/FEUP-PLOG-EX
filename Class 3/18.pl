862943125626985e1b5ba639f743a1723bfe9f26
Henrique Pereira <henriquemrp2001@gmail.com>
2020-11-27 21:11:20 +0000

    Class 3 - 18

---
+++ Class 3/18.pl
@@ -0,0 +1,19 @@
+% a)
+duplicate([], []).
+duplicate([H | T], [H, H | L]) :-
+    duplicate(T, L).
+
+% b)
+multiply([], _, []).
+multiply([H | T], N, [H | L]) :-
+    N_ is N - 1,
+    repeat(H, N_, L, Res),
+    multiply(T, N, Res).
+
+repeat(_, 0, L, Res) :-
+    Res = L.
+repeat(H, N, [H | T], Res) :-
+    N_ is N - 1,
+    repeat(H, N_, T, Res).
+
+    
