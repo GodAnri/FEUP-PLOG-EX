07a77dcb7fbf6fa35480a32cf93d3ade29fd9901
Henrique Pereira <henriquemrp2001@gmail.com>
2021-01-07 08:45:02 +0000

    Corrected exercise 2 of class 2

--- Class 2/2.pl
+++ Class 2/2.pl
@@ -1,24 +1,36 @@
-% ?- a(X,2).
-% X = A
-
-% ?- b(X,Kalamazoo).
-% no
-
-% ?- c(X,b3).
-% X = a3
-
-% ?- c(X,Y).
-% X = a1; Y = b1
-
-% ?- d(X,Y).
-% no
-
 a(a1,1).
-a(a,2).
-a(a3,n).
+a(A,2).
+a(a3,N).
 b(1,b1).
-b(2,b).
-b(n,b3).
+b(2,B).
+b(N,b3).
 c(X,Y) :- a(X,N), b(N,Y).
 d(X,Y) :- a(X,N), b(Y,N).
-d(X,Y) :- a(N,X), b(N,Y). 
+d(X,Y) :- a(N,X), b(N,Y).
+
+% ?- a(X,2).
+% yes
+
+% ?- b(X,Kalamazoo).
+% X = 2.
+
+% ?- c(X,b3).
+% X = a1;
+% yes.
+
+% ?- c(X,Y).
+% X = a1, Y = b1;
+% X = a1, Y = b3;
+% yes.
+
+% ?- d(X,Y).
+% X = a1, Y = 2;
+% Y = 2;
+% X = a3, Y = 1;
+% X = a3, Y = 2;
+% X = a3;
+% X = 1, Y = b3;
+% X = 2, Y = b1;
+% X = 2;
+% X = 2, Y = b3;
+% Y = b3.
