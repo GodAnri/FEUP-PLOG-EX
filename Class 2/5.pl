49963a9cb54f973287d53589736e0b0d481177e1
Henrique Pereira <henriquemrp2001@gmail.com>
2021-01-07 08:47:24 +0000

    Corrected exercise 5 of class 2

--- Class 2/5.pl
+++ Class 2/5.pl
@@ -3,8 +3,8 @@ e_primo(3).
 e_primo(N) :-
     N > 3,
     N mod 2 =\= 0,
-    \+div(N,3).
-div(N,Factor) :-
+    \+divi(N,3).
+divi(N,Factor) :-
     N mod Factor =:= 0.
-div(N,Factor) :-
-    Factor * Factor < N, Next is Factor + 2, div(N, Next).
+divi(N,Factor) :-
+    Factor * Factor < N, Next is Factor + 2, divi(N, Next).
